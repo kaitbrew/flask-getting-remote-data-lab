f1346c1e4ede1ba37370b348ba576555206a9513
Kaitlyn Brewer <kaitlynmbrewer@gmail.com>
2025-11-05 19:38:51 -0500

    both tests passing

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,10 +7,9 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        url="https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
-        response=requests.get(url)
+        response=requests.get(self.url)
         return response.content
 
     def load_json(self):
-        formatted_response=f"name: {response['docs'][0]['title']}\noccupation: {response['docs'][0]['author_name'][0]}"
-        return formatted_response
+        response=requests.get(self.url)
+        return response.json()
